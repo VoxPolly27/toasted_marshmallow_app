1ed044bb599938aadc46abf5d70e3e70aa0e70c7
VoxPolly27 <39667433+VoxPolly27@users.noreply.github.com>
2020-08-16 20:08:56 +0200

    Upadated App.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var port = process.env.PORT || 5000;
+//var port = process.env.PORT || 5000;
 
 var app = express();
 

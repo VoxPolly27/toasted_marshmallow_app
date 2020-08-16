8131d9468c615ee514f14573a787dd9dde4e66fe
VoxPolly27 <39667433+VoxPolly27@users.noreply.github.com>
2020-08-16 20:32:27 +0200

    Upadated App.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-//var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 
 var app = express();
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());

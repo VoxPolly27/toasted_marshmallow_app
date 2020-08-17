2ac25b33740e2484ef65472318abee38a6ad6dad
VoxPolly27 <39667433+VoxPolly27@users.noreply.github.com>
2020-08-17 20:51:37 +0200

    Updates

---
+++ precache-manifest.17937ba9f7fdf8ce7a62dc49fb84a1e2.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9212f00a139a8535872c0a3fb3bd4d9b",
+    "url": "/toasted_marshmallow_app/index.html"
+  },
+  {
+    "revision": "cf982a21f791680c63a0",
+    "url": "/toasted_marshmallow_app/static/css/main.a8b620a6.chunk.css"
+  },
+  {
+    "revision": "6f3f953ab53cfa0da163",
+    "url": "/toasted_marshmallow_app/static/js/2.c3514772.chunk.js"
+  },
+  {
+    "revision": "bf71ac297fcfe9a3d2d199a48fc3f60c",
+    "url": "/toasted_marshmallow_app/static/js/2.c3514772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf982a21f791680c63a0",
+    "url": "/toasted_marshmallow_app/static/js/main.5be3e8d9.chunk.js"
+  },
+  {
+    "revision": "13ae98c1fb66d53ed8e1",
+    "url": "/toasted_marshmallow_app/static/js/runtime-main.82a23354.js"
+  },
+  {
+    "revision": "1b86ec79aec863cb4fa526ce384efe1d",
+    "url": "/toasted_marshmallow_app/static/media/Comet.1b86ec79.svg"
+  },
+  {
+    "revision": "5bdd214c8dd18a5d50cd9edeab97370b",
+    "url": "/toasted_marshmallow_app/static/media/Cosmic.5bdd214c.svg"
+  },
+  {
+    "revision": "87891b6b7348ce60ae8f88e9a0a4ef6e",
+    "url": "/toasted_marshmallow_app/static/media/Icon feather-smile.87891b6b.svg"
+  },
+  {
+    "revision": "30512ea8331c436320c639b70d47f9e7",
+    "url": "/toasted_marshmallow_app/static/media/Icon-behance.30512ea8.svg"
+  },
+  {
+    "revision": "d3284aef79045c302507bf3c9e99d4f9",
+    "url": "/toasted_marshmallow_app/static/media/Icon-github.d3284aef.svg"
+  },
+  {
+    "revision": "4314bb0483a8a9775f5e054dcb85c48b",
+    "url": "/toasted_marshmallow_app/static/media/Logo.4314bb04.svg"
+  },
+  {
+    "revision": "453694fa687a5323be7569153bde547e",
+    "url": "/toasted_marshmallow_app/static/media/LogoLight.453694fa.svg"
+  },
+  {
+    "revision": "d1cfec30b3a011acce2cc647fd338309",
+    "url": "/toasted_marshmallow_app/static/media/MarshMallow.d1cfec30.svg"
+  },
+  {
+    "revision": "61536767171b9ec82a9317ad07f3e030",
+    "url": "/toasted_marshmallow_app/static/media/Mobile.61536767.svg"
+  },
+  {
+    "revision": "7eba68129a58e9575cdf4f7cfe0487d5",
+    "url": "/toasted_marshmallow_app/static/media/Prism.7eba6812.svg"
+  },
+  {
+    "revision": "4481c905d0636e5f708ab4598e315dbd",
+    "url": "/toasted_marshmallow_app/static/media/UI_UX.4481c905.svg"
+  },
+  {
+    "revision": "ca5947329dfc0fd9550915e63b54a8eb",
+    "url": "/toasted_marshmallow_app/static/media/icon-apps.ca594732.svg"
+  },
+  {
+    "revision": "c3d235e9b368a1e72c984b97bc81e1ec",
+    "url": "/toasted_marshmallow_app/static/media/icon-cloud.c3d235e9.svg"
+  },
+  {
+    "revision": "ac0f307dd14508bf9d054e7a253145fc",
+    "url": "/toasted_marshmallow_app/static/media/icon-figma.ac0f307d.svg"
+  },
+  {
+    "revision": "3c517305d2d83951d354367f4225b0f4",
+    "url": "/toasted_marshmallow_app/static/media/icon-iot.3c517305.svg"
+  },
+  {
+    "revision": "99b03abc61d616997d6ae1a937fcd0a0",
+    "url": "/toasted_marshmallow_app/static/media/icon-linkedin.99b03abc.svg"
+  },
+  {
+    "revision": "a194bedaaff7fc3e9c12f7e195bab898",
+    "url": "/toasted_marshmallow_app/static/media/icon-mobile.a194beda.svg"
+  },
+  {
+    "revision": "1bb0bb88fb0afa953f7e21b7d384f1ce",
+    "url": "/toasted_marshmallow_app/static/media/icon-sketch.1bb0bb88.svg"
+  },
+  {
+    "revision": "74563a547762f9cf2305965e15c8a990",
+    "url": "/toasted_marshmallow_app/static/media/icon-software.74563a54.svg"
+  },
+  {
+    "revision": "35baec848d5bdc9da3eefd770137dfe6",
+    "url": "/toasted_marshmallow_app/static/media/icon-ui.35baec84.svg"
+  },
+  {
+    "revision": "f2660a9724a13f19fa1e15647f0a9ef0",
+    "url": "/toasted_marshmallow_app/static/media/icon-web.f2660a97.svg"
+  },
+  {
+    "revision": "f704da96aff46d3015a2186580badf3b",
+    "url": "/toasted_marshmallow_app/static/media/icon-xd.f704da96.svg"
+  },
+  {
+    "revision": "344604b186d6a8d4818e48445d29aee0",
+    "url": "/toasted_marshmallow_app/static/media/layer-1.344604b1.svg"
+  },
+  {
+    "revision": "d0a4b7e0500ab69a7ef7ac8ba56afee9",
+    "url": "/toasted_marshmallow_app/static/media/layer-2.d0a4b7e0.svg"
+  }
+]);

6d682fc1180822b1c8c2a9d30ba7b9b94211ef6d
VoxPolly27 <39667433+VoxPolly27@users.noreply.github.com>
2020-08-17 19:37:17 +0200

    removed reactstrap from layouts

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { Container } from 'reactstrap';
 import { NavMenu } from 'components/layout-elements/navigation/navigation-menu';
 import FloatingButtons from '../layout-elements/floating-buttons/floating-buttons';
 
